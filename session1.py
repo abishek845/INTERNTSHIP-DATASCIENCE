dd263d438c7b14ad060f074a1498dd8acd81e6b5
abishekpandian <abishekpandian2003@gmail.com>
2025-10-08 16:54:24 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,27 @@
+#task-1
+num=10;
+if num%2==0:
+    print("the number is even");
+else:
+    print("the number is odd");
+
+#task-2
+age=58;
+if age>=18:
+          print("you can vote in the comming election");
+else:
+        print("yopu cannot vote for comming election");
+
+#task-3
+n=5;
+if n%3==0 and n%5==0:
+    print("FIZZ,Buzz");
+elif n%3==0:
+    print("fizz");
+elif n%5==0:
+    print("buzz");
+else:
+    print("not divisible");
+
+    
+          
